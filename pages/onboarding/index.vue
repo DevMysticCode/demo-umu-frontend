492df9532176d306fa421488520a703f54d30165
Shaun <shaun.underwood@bamgsports.com>
2025-08-28 21:30:50 +0100

    Remove unused CSS for onboarding background image to streamline the component

--- pages/onboarding/index.vue
+++ pages/onboarding/index.vue
@@ -34,29 +34,3 @@ const onSkipClick = () => {
   alert('skip')
 }
 </script>
-
-<style>
-.backgound-image {
-  background-image: url('/public/images/onboarding_background.jpg');
-  background-size: cover;
-  background-position: center;
-  position: relative;
-}
-
-.backgound-image::before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.6);
-  pointer-events: none;
-  z-index: 1;
-}
-
-.backgound-image > * {
-  position: relative;
-  z-index: 2;
-}
-</style>
